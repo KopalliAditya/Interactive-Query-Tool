f65070ff90a6704bf9bf4f4fc7e2b0cfe5c1de95
Aditya Kopalli <kopalliaditya96@gmail.com>
2022-12-19 00:38:39 -0800

    Adding libraries

---
+++ QueryTool/QueryTool/CPUUsage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    internal class CPUUsage
+    {
+        public string? TimeStamp { get; set; }
+
+        public string? IP { get; set; }
+
+        public string? CPU_ID { get; set; }
+
+        public string? Usage { get; set; }
+    }
+}
--- QueryTool/QueryTool/Program.cs
+++ QueryTool/QueryTool/Program.cs
@@ -1,2 +1,25 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using CsvHelper;
+using System.Globalization;
+using Sharprompt;
+using ConsoleApp;
+
+
+IEnumerable<CPUUsage> records;
+
+using (var reader = new StreamReader("C:\\Users\\Aditya\\source\\repos\\ConsoleApp\\ConsoleApp\\CPUUsage.csv"))
+using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+{
+    records = csv.GetRecords<CPUUsage>();
+}
+
+var name = Prompt.Input<string>("What's your name?");
+Console.WriteLine($"Hello, {name}!");
+
+// Password input
+var secret = Prompt.Password("Type new password", validators: new[] { Validators.Required(), Validators.MinLength(8) });
+Console.WriteLine("Password OK");
+
+// Confirmation
+var answer = Prompt.Confirm("Are you ready?", defaultValue: true);
+Console.WriteLine($"Your answer is {answer}");
